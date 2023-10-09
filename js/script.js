e323dbc26ba25b2bd5f2182e587718a219dfec38
Vanesa Suarez <vanesuarez2301@gmail.com>
2023-10-08 21:17:16 -0300

    design changes

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
             
             container.innerHTML = ""
 
-            // AGREGAR ACA FUNCION PARA MOSTRAR LAS PELICULAS
+            // Funcion para mostrar las peliculas
 
             filteredMovies.forEach( x => {
 
@@ -44,27 +44,23 @@ document.addEventListener("DOMContentLoaded", () => {
               const vote = x.vote_average;
               const id = x.id;
 
-              const tarjetaBtn = document.createElement("button"); // crea un botón para cada pelicula del array filtrado
-              tarjetaBtn.setAttribute("id", id); // agregamos la id de la pelicula al elemento
-              tarjetaBtn.setAttribute("data-bs-toggle", "offcanvas"); // agrega atributos de boostrap para que funcione el top
-              tarjetaBtn.setAttribute("data-bs-target", "#offcanvasTop");
-              tarjetaBtn.setAttribute("aria-controls", "offcanvasTop"); 
+              const btnCard = document.createElement("button"); // crea un botón para cada pelicula del array filtrado
+              btnCard.setAttribute("id", id); // agregamos la id de la pelicula al elemento
+              btnCard.setAttribute("data-bs-toggle", "offcanvas"); // agrega atributos de boostrap para que funcione el top
+              btnCard.setAttribute("data-bs-target", "#offcanvasTop");
+              btnCard.setAttribute("aria-controls", "offcanvasTop"); 
 
-              const tarjeta = document.createElement("li"); // crea un elemento li
-              tarjeta.classList.add("list-group-item"); // agrega clases boostrap
-              tarjeta.classList.add("d-flex") ;
-              tarjeta.classList.add("justify-content-between");
-              tarjeta.classList.add("bg-dark");
-              tarjeta.classList.add("text-white");
-              tarjeta.innerHTML = // agrega contenido a la tarjeta de la pelicula, nombre y descripción 
+              const card = document.createElement("li"); // crea un elemento li
+              card.classList.add("list-group-item", "d-flex", "justify-content-between", "bg-dark", "text-white");
+              card.innerHTML = // agrega contenido a la tarjeta de la pelicula, nombre y descripción 
               `<div class="p-2 bd-highlight">
               <h5> ${title}</h5>
-              <p> ${tagline}</p>
+              <p><i> ${tagline}</i></p>
               </div>`
 
               // if que agrega estrellas segun su puntaje
               if (vote<=10 && vote>8) { // muestra las 5 estrellas
-                tarjeta.innerHTML += `<div class="ms-auto p-2 bd-highlight">
+                card.innerHTML += `<div class="ms-auto p-2 bd-highlight">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
@@ -72,7 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <span class="fa fa-star checked"></span>
               </div>`
               } else if (vote<=8 && vote>6) { // muestra 4 estrellas de 5
-                tarjeta.innerHTML += `<div class="ms-auto p-2 bd-highlight">
+                card.innerHTML += `<div class="ms-auto p-2 bd-highlight">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
@@ -80,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <span class="fa fa-star"></span>
               </div>`
               } else if (vote<=6 && vote>4) { // muestra 3 estrellas de 5
-                tarjeta.innerHTML += `<div class="ms-auto p-2 bd-highlight">
+                card.innerHTML += `<div class="ms-auto p-2 bd-highlight">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
@@ -88,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <span class="fa fa-star"></span>
               </div>`
               } else if (vote<=4 && vote>2) { // muestra 2 estrellas de 5
-                tarjeta.innerHTML += `<div class="ms-auto p-2 bd-highlight">
+                card.innerHTML += `<div class="ms-auto p-2 bd-highlight">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star"></span>
@@ -96,7 +92,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <span class="fa fa-star"></span>
               </div>`
               } else if (vote<=2 && vote>0) { // muestra 1 estrella de 5
-                tarjeta.innerHTML += `<div class="ms-auto p-2 bd-highlight">
+                card.innerHTML += `<div class="ms-auto p-2 bd-highlight">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star"></span>
                 <span class="fa fa-star"></span>
@@ -105,13 +101,13 @@ document.addEventListener("DOMContentLoaded", () => {
               </div>`
               }
 
-              lista.appendChild(tarjetaBtn);
-              tarjetaBtn.appendChild(tarjeta);
+              container.appendChild(btnCard);
+              btnCard.appendChild(card);
 
               // FIN parte 2
 
               //Punto 3, Cuando el usuario pulse en alguna de las películas mostradas, se deberá desplegar un contenedor superior con la siguiente información de dicha película: title, overview y lista de genres.
-              tarjetaBtn.addEventListener("click", () => {
+              btnCard.addEventListener("click", () => {
                 const titleM = document.getElementById('offcanvasTopLabel'); // selecciona los elementos de contenedor top que se desplegara
                 const overviewM = document.getElementById('overviewM');
                 const genresM = document.getElementById('genresM');
@@ -131,6 +127,7 @@ document.addEventListener("DOMContentLoaded", () => {
               // Punto 4, Añadir en lo anterior un botón con un desplegable que contenga el año de lanzamiento (sin el mes ni el día), la duración del largometraje, el presupuesto con el que contó y las ganancias obtenidas
 
               genresM.innerHTML += // agrega el boton final con la info extra de la pelicula
+              
                `
               <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
